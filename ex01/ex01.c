21f5b330853a94243e98358cb389095e3e74dc92
João <joaopaulobastos@proton.me>
2026-03-13 10:27:45 -0400

    exercício 1 da aula 01

---
+++ ex01/ex01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int soma(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int numero1, numero2;
+
+    printf("Insira o valor do primeiro número inteiro: ");
+    scanf("%d", &numero1);
+
+    printf("Insira o valor do segundo número inteiro: ");
+    scanf("%d", &numero2);
+
+    printf("A soma dos dois valores é %d", soma(numero1, numero2));
+
+    return 0;
+}
